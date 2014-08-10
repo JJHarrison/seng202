f6d0857b7b4f055bd0812215bb596521c808d279
sds53 <sds53@uclive.ac.nz>
2014-08-11 10:35:17 +1200

    Prototype that i worked on over the weekend Needs comments Disntance function doesnt work properly yet other stuff might not work properly

---
+++ SENG202/src/fileHandler/FileLoader.java
@@ -0,0 +1,98 @@
+package fileHandler;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class FileLoader {
+	ArrayList<Event> events = new ArrayList<Event>();
+	
+	public static void main(String[] args) {
+		FileLoader fl = new FileLoader();
+		fl.load();
+		System.out.println("File Loaded\n");
+		
+		//print out events and their date
+		for(int i = 0; i < fl.events.size(); i++){
+			System.out.println("Speed is: " + fl.events.get(i).points.get(3).speed);
+			System.out.println("Time is: " + fl.events.get(i).points.get(3).dTime);
+			System.out.println("distance is: " + fl.events.get(i).points.get(3).distance);
+			//System.out.println(fl.events.get(i).points.get(3).dTime);
+			System.out.println(fl.events.get(i).eventName + "\n");
+		}
+		
+		
+	}
+	
+	
+	public void load() {
+		String file = "/Users/SamSchofield/Desktop/UNI/semester_2/SENG_202/Assignment/Data/seng202_2014_example_data.csv";
+		BufferedReader br = null;
+		String line = "";
+		String split = ",";
+		Event currentEvent = new Event("");
+		DataPoint lastPoint = new DataPoint("0", "0:0:0", "0", "0", "0", "0", 0, 0);
+		
+		try {
+			br = new BufferedReader(new FileReader(file));
+			while((line = br.readLine()) != null) {
+				String[] dataLine = line.split(split);
+				
+				if(dataLine[0].contains("#start")){
+					currentEvent = new Event(dataLine[1]);
+					events.add(currentEvent);
+				} else {
+					double distance = getDistance(dataLine[3], dataLine[4], lastPoint.lat, lastPoint.lon);
+					int time = getTime(dataLine[1], lastPoint.time);
+					DataPoint p = new DataPoint(dataLine[0], dataLine[1], dataLine[2], dataLine[3], dataLine[4], dataLine[5], distance, time);
+					lastPoint = p;
+					currentEvent.points.add(p);
+				}
+				
+				
+			}
+		} catch (FileNotFoundException e){ 
+			e.printStackTrace();
+			System.out.println("File Not Found");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("couldnt read line");
+		}
+	}
+	
+	private int getTime(String currentTime, String lastTime) {
+		// returns the time difference between the previous and last data points
+		// need to fix so that it works over midnight
+		int dTime = 0;
+		String[] cTime = currentTime.split(":");
+		String[] lTime = lastTime.split(":");
+		
+		int cTimeInSec = (Integer.parseInt(cTime[0]) * 60 * 60) + (Integer.parseInt(cTime[1]) * 60) + (Integer.parseInt(cTime[2]));
+		int lTimeInSec = (Integer.parseInt(lTime[0]) * 60 * 60) + (Integer.parseInt(lTime[1]) * 60) + (Integer.parseInt(lTime[2]));
+		
+		dTime = cTimeInSec - lTimeInSec;
+		return dTime;
+	}
+	
+	private double getDistance(String currentLat, String currentLon, String lastLat, String lastLon) {
+		// returns the distance between the current data point and the previous data point
+		// this doesn't  give the right distance 
+		double distance = 0;
+		double radius = 6373;
+		double lat2 = Double.parseDouble(lastLat);
+		double lat1 = Double.parseDouble(currentLat);
+		
+		double dlon = Float.parseFloat(currentLon) - Float.parseFloat(lastLon);
+		double dlat = lat1 - lat2; 
+		
+		double a = Math.pow(Math.sin(dlat / 2), 2) + (Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2),2));
+		double c = 2 * Math.atan2( Math.sqrt(a), Math.sqrt(1-a) );
+		distance = radius * c;
+		
+		return distance;
+	}
+
+}
