123fa22c0870b9ccb3a6c551a4922418b07a00c3
daniel <daniel@daniel>
2014-08-11 00:31:14 +1200

    Just trying out this newfangled autocomplete! :)

--- SENG202/src/test/MyClass.java
+++ SENG202/src/test/MyClass.java
@@ -3,6 +3,12 @@ package test;
 public class MyClass {
 //change made
 	public static void main(String[] argv) {
-		System.out.print(false);{};
+		System.out.print(false);
+		System.out.println(true);
+	}
+	
+	@Deprecated
+	public static void print__() {
+		System.out.print(false);
 	}
 }
