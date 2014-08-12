18d8e7c1f99c4a7d5fdb98b58b0f88fc4a441f95
simoncrequer <sjc291@uclive.ac.nz>
2014-08-12 12:01:05 +1200

    wrote EventContainerTest

--- SENG202/src/tests/EventContainerTest.java
+++ SENG202/src/tests/EventContainerTest.java
@@ -1,11 +1,24 @@
 package tests;
 
 import junit.framework.TestCase;
+import dataModel.EventContainer;
+import dataModel.Event;
 
 public class EventContainerTest extends TestCase {
+	
+	private EventContainer ec;
 
 	protected void setUp() throws Exception {
 		super.setUp();
+		
+		ec = new EventContainer();
+	}
+	
+	public void testAddEvent() {
+		Event e = new Event("Test Event");
+		ec.addEvent(e);
+		
+		assertEquals(ec.getEvents().get(0), e);
 	}
 
 }
