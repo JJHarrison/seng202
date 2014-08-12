9faebf8a3a4ec9456e2166d1f4b8e97fe35e556c
jjharrison-uc <jjh72@uclive.ac.nz>
2014-08-12 14:43:05 +1200

    user model in process

--- SENG202/src/userModel/UserProfile.java
+++ SENG202/src/userModel/UserProfile.java
@@ -1,5 +1,32 @@
 package userModel;
 
-public class UserProfile {
+import java.util.Calendar;
 
+import dataModel.EventContainer;
+
+public class UserProfile {
+	private Calendar dateofBirth;
+	private String name;
+	private double weight;
+	private double height;
+	private Gender gender;
+	private double BMI;
+	private EventContainer events;
+	public enum Gender {
+		MALE, FEMALE
+	}
+	
+
+	public UserProfile(String name, Calendar dateOfBirth, double weight, double height, Gender gender){
+		this.dateofBirth = dateOfBirth;
+		this.name = name;
+		this.weight = weight;
+		this.height = height;
+		this.gender = gender;
+		this.BMI = calculateBMI();
+		this.events = new EventContainer();
+		}
+	
+	
+	
 }
