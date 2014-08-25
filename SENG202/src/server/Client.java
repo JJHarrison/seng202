1b9cbb6545b46255ee6ed1129ebe95b2635d2112
jjharrison-uc <jjh72@uclive.ac.nz>
2014-08-25 16:55:04 +1200

    Reworked the server and client classes

--- SENG202/src/server/Client.java
+++ SENG202/src/server/Client.java
@@ -1,46 +1,123 @@
 package server;
 
 import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.io.*;
 public class Client {
+	private Socket clientSocket;
+	private ObjectInputStream input;
+	private ObjectOutputStream output;
+	private final int portNumber = 8888;
+	private String hostName = "LocalHost";
 	
+	/**
+	 * Sets up a connection with the server.	
+	 */
+	public void setupConnection(){
+		try{
+			System.out.println(startMessage() + " attempting to start a connection to [" + hostName + 
+					"] on port [" + portNumber + "]...");
+			clientSocket = new Socket(hostName, portNumber);
+			setupStreams();
+			System.out.println(startMessage() + " connection accepted by [" + hostName + "]\n");
+		}
+		catch(IOException e){
+			e.printStackTrace();
+			System.out.println(startMessage() + " uh oh something wrong happened");
+		}
+	}
+	
+	/**
+	 * Sets up the input/output streams for sending and receiving information with the server
+	 * @throws IOException
+	 */
+	public void setupStreams() throws IOException{
+		output = new ObjectOutputStream(clientSocket.getOutputStream());
+		output.flush();
+		input = new ObjectInputStream(clientSocket.getInputStream());
+	}
+	
+	/*
+	 * Will need to add most of the functionality for sending events to this function
+	 * Probably will need a few more exceptions for wrong object or whatever
+	 * 
+	 */
+	/**
+	 * To transfer information to the server.
+	 * @param message
+	 */
+	public void transferToServer(Object message){
+		try{
+			System.out.println(startMessage() + " attempting to send [" + message + "]...");
+			output.writeObject(message);
+			readConfirmation();
+			System.out.println(startMessage() + " succesful!\n");
+		}
+		catch(IOException e){
+			System.out.println(startMessage() + " FAILED!!\n");
+		}
+	}
+	
+	/**
+	 * Reads and displays the confirmation that the server has correctly received the information sent from the client 
+	 * @throws IOException
+	 */
+	public void readConfirmation() throws IOException{
+		String buf;
+		try {
+			buf = (String)input.readObject();
+			if(buf != null){
+				System.out.println("[" + getCurrentTime() + "]<Server> Responded with [" + buf + "].");
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Closes the I/O Streams and the connection to the server
+	 */
+	public void closeStuff(){
+		try {
+			System.out.println(startMessage() + " closing connection...;");
+			output.writeObject("END");
+			input.close();
+			output.close();
+			clientSocket.close();
+			System.out.println(startMessage() + " connection closed()");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/**
+	 * Gets the current time on the client formated to HH:mm:SS
+	 * @return String of the current time of the client.
+	 */
+	public String getCurrentTime(){
+		SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
+		Calendar cal = Calendar.getInstance();
+		return df.format(cal.getTime());
+	}
+	
+	/**
+	 * Creates a message consisting of [currentTime]<Client> for the client output messages.
+	 * @return The opening string for client output messages.
+	 */
+	public String startMessage(){
+		return "[" + getCurrentTime()+ "]<Client>";
+	}
 	
 	
 	public static void main(String[] args){
-		
-	//port number and host name	
-	int portNumber = 8888;
-	String host = "localhost";
-	
-	//try to open a connection and send the string "Hello World"
-	try {
-		//message to show that the client is trying to conact the server
-		System.out.println("Client will attempt to contact server " + host + " on port " + portNumber);
-		
-		//socket that will be used for the connection
-		Socket skt = new Socket(host, portNumber);
-		
-		//once a connection is established uses some streams
-		BufferedReader myInput = new BufferedReader(new InputStreamReader(skt.getInputStream()));
-		PrintStream myOutput = new PrintStream(skt.getOutputStream());
-		
-		//write something to the server
-		myOutput.print("Hello World\n"); //string that is sent to the server
-		String buf = myInput.readLine();
-		if(buf != null){
-			System.out.println("The server responded with [" + buf + "]");
-		}
-		
-		//close the socket
-		skt.close();
-		System.out.println("Client is exiting");
-			
-		}
-	
-	catch (IOException ex) {
-		ex.printStackTrace();
-		System.out.println("Uh oh something wrong happened");
-	}
-	
+		Client c = new Client();
+		c.setupConnection();
+		//Just test strings at the moment, will need to build on this when we know what we are actually sending
+		c.transferToServer("Message one");
+		c.transferToServer("Message two");
+		c.transferToServer("Message three");
+		c.closeStuff();	
 }
 }
--- SENG202/src/server/Server.java
+++ SENG202/src/server/Server.java
@@ -1,53 +1,158 @@
 package server;
 
 import java.net.*;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.io.*;
+
+import com.sun.xml.internal.ws.resources.SenderMessages;
 
 
 public class Server {
+	private ServerSocket server;
+	private Socket connection;
+	private ObjectInputStream input;
+	private ObjectOutputStream output;
+	final private int portNumber = 8888; 
+	final private int maxQueue = 10;
+	
+	/*
+	 * Still need to look into threading for the client sockets
+	 */
+		
+	/**
+	 * Starts a server that will endlessly wait for connections
+	 */
+	public void startServer(){
+		try{
+			server = new ServerSocket(portNumber, maxQueue);
+			while(true){
+				try{
+					waitForConnection();
+					setupStreams();
+					whileTransfering();
+					System.out.println(startMessage() + " Connection ended!");
+					System.out.println("-----------------------------------"
+							+ "--------------------");
+				}
+				catch(EOFException e){
+					System.out.println(startMessage() + " Connection ended!");
+					System.out.println("-----------------------------------"
+							+ "--------------------");
+				}
+				finally{
+					closeStuff();
+				}
+			}
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Waits for a connection to be formed with the client.
+	 * @throws IOException
+	 */
+	public void waitForConnection() throws IOException{
+		System.out.println(startMessage() + " Waiting for a client to connect...");
+		//Once a client connects a socket is open for the server and client
+		connection = server.accept();
+		System.out.println(String.format(startMessage() + " Now connected to %s", connection.getInetAddress().getHostName()));
+	}
+	
+	/**
+	 * Sets up the output/input streams to send/receive objects with the client.
+	 * @throws IOException
+	 */
+	public void setupStreams() throws IOException{
+		output = new ObjectOutputStream(connection.getOutputStream());
+		output.flush();
+		input = new ObjectInputStream(connection.getInputStream());
+		System.out.println(startMessage() + " Streams are now ready to be used");
+	}
+	
+	/*
+	 *This will need to be changed once we are sending actual information that the server will store 
+	 */
+	public void whileTransfering() throws IOException{
+		String clientMessage = null;
+		System.out.println(startMessage() + " Ready for transfer...\n");
+		do{
+		try{
+			//Will need to send a string to from the client describing what is being sent
+			//maybe a comment that would also be stored with the file
+			clientMessage = (String)input.readObject();
+			//Prints that we have received the object
+			System.out.println(startMessage() + " User sent: " + clientMessage);
+			/*
+			 * This is where we need to process the file being sent from the client...
+			 * --We must decide where it will be stored
+			 * --We must decide how it will be stored
+			 * --Probably need to have some container/storage class
+			 * --I think we will also need some checks here, or decide if that client will perform a check also/instead
+			 */
+			//send a confirmation to the client that the message was received correctly
+			sendMessage(clientMessage);
+		}
+		catch(ClassNotFoundException e){
+			System.out.println(startMessage() + " I don't know what to do with that type of object");
+		}
+		}
+		while(!clientMessage.equals("END"));
+	}
+	
+	/**
+	 * Closes the streams and socket once the connection is lost
+	 */
+	public void closeStuff(){
+		try{
+			input.close();
+			output.close();
+			connection.close();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Sends a confirmation message to the client that their message was received.
+	 * @param message The message that was received from the client.
+	 */
+	private void sendMessage(String message){
+		try{
+			output.writeObject(message);
+			output.flush();
+			System.out.println(startMessage() + " Sent confirmation to the <Client>\n");
+		}
+		catch(IOException e){
+			System.out.println(startMessage() + " No confirmation was sent to the <Client>");
+		}
+	}
+	
+	/**
+	 * Gets the current time on the server.
+	 * @return
+	 */
+	public String getCurrentTime(){
+		SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
+		Calendar cal = Calendar.getInstance();
+		return df.format(cal.getTime());
+	}
+	
+	/**
+	 * Creates a message consisting of [currentTime]<Server> for the server output messages.
+	 * @return The opening string for server output messages.
+	 */
+	public String startMessage(){
+		return "[" + getCurrentTime()+ "]<Server>";
+	}
 	
 	public static void main(String[] args){
-	
-	//port number for the socket
-	final int portNumber = 8888;
-	
-	try {
-		//the socket to be used for server
-		ServerSocket myServerSocket = new ServerSocket(portNumber);
-		
-		//wait for incoming connection
-		System.out.println("Server is waiting for an incoming connection to host = "
-				+ InetAddress.getLocalHost().getCanonicalHostName()
-				+ ", port = " + myServerSocket.getLocalPort());
-		//blocks the server until a client makes a connection to this port
-		Socket skt = myServerSocket.accept();
-		
-		//connection established, using java.io to help transfer information
-		BufferedReader myInput = new BufferedReader(new InputStreamReader(skt.getInputStream()));
-		PrintStream myOutput = new PrintStream(skt.getOutputStream());
-		
-		
-		//attempt to read from the socket connection
-		String buf = myInput.readLine();
-		
-		//prints the message that was sent to the server
-		if(buf != null){
-			System.out.println("Server has read [" + buf + "]" );
-			myOutput.print("Got it");
-		}
-		
-		//close connection
-		skt.close();
-		System.out.println("Server is exiting");
-		myServerSocket.close();
-		
-	}
-	
-	catch (IOException ex) {
-		ex.printStackTrace();
-		System.out.println("Whoops, something bad has happened!");
-	}
-	
+		Server s = new Server();
+		s.startServer();
 	
 }
 }
