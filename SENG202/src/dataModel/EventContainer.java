304bfb142f95db9a46e0e0d204956cd9c43c69a0
simoncrequer <sjc291@uclive.ac.nz>
2014-08-29 15:12:36 +1200

    updated EventContainer to store events in days rather than one huge list. - simon

--- SENG202/src/dataModel/EventContainer.java
+++ SENG202/src/dataModel/EventContainer.java
@@ -3,22 +3,25 @@ package dataModel;
 
 import dataModel.Event;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedList;
 
 /**
  * This class is abstract version of an activity event container. The activity event container consists of
- * activity event objects representing activity events imported from a fitness tracking device.
+ * activity days containing Event objects representing activity events imported from a fitness tracking device.
  * @author Fitr.Team
  */
 public class EventContainer {
-	private HashMap<String, ArrayList<Event>> days;
+	private HashMap<String, LinkedList<Event>> days;
 	
 	/**
-	 * Constructs a new event container object consisting of an arraylist to contain activity events.
+	 * Constructs a hash map to store the activity events for each day in.
 	 */
 	public EventContainer() {
-		days = new HashMap<String, ArrayList<Event>>();
+		days = new HashMap<String, LinkedList<Event>>();
 	}
 	
 	/**
@@ -26,14 +29,50 @@ public class EventContainer {
 	 * @param e The activity event to be added to the container.
 	 */
 	public void addEvent(Event e) {
-		this.events.add(e);
+		String dateString = dateString(e.getStartTime().getTime());
+		if (days.containsKey(dateString)) {
+			// find the correct position to add the event
+			LinkedList<Event> day = days.get(dateString);
+			for (int i = 0; i < days.size(); i++) {
+				// reached the right index.
+				if (e.getStartTime().before(day.get(i).getStartTime())) {
+					// add e at i
+					day.add(i, e);
+				}
+				// e is the latest event
+				else if (i == days.size() - 1) {
+					// add e to the end of the list
+					day.add(e);
+				}
+			}
+		} else {
+			LinkedList<Event> day = new LinkedList<Event>();
+			day.add(e);
+			days.put(dateString, day);
+		}
 	}
 	
 	/**
-	 * Returns the event container array that contains the activity events.
-	 * @return The array containing activity events
+	 * Returns the linked list that contains the activity events for the day.
+	 * @param date The day that the caller wants the activity events for.
+	 * @return The linked list containing activity events
 	 */
-	public ArrayList<Event> getEvents() {
-		return this.events;
+	public LinkedList<Event> getEvents(Date date) {
+		String dateString = dateString(date);
+		if (days.containsKey(dateString)) {
+			return days.get(dateString);
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * Returns a date string in the format dd/mm/yyyy to be used as the key for
+	 * the hash map.
+	 * @param date The date object to be formatted
+	 * @return The formatted date string
+	 */
+	private String dateString(Date date) {
+		return new SimpleDateFormat("dd/MM/yyyy").format(date.getTime());
 	}
 }
