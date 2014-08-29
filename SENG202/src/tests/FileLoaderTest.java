34e323101d24b11a236445fc2841b3e7be5bad3f
simoncrequer <sjc291@uclive.ac.nz>
2014-08-29 12:32:49 +1200

    added FileLoaderTest -simon

---
+++ SENG202/src/tests/FileLoaderTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests for the FileLoader class functionality
+ * @author Simon
+ *
+ */
+public class FileLoaderTest extends TestCase {
+
+	public FileLoaderTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
