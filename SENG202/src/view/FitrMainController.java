693d1a608753278f1cc72de9373d572caa374d88
daniel <daniel@daniel>
2014-08-31 12:26:31 +1200

    Formatted whole project

--- SENG202/src/view/FitrMainController.java
+++ SENG202/src/view/FitrMainController.java
@@ -4,19 +4,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
-
-
 public class FitrMainController {
-	
+
 	@FXML
 	GridPane selectionGrid;
-	
+
 	Label l = new Label();
-	
+
 	@FXML
 	private void initialize() {
 	}
-	
-	
-}
 
+}
