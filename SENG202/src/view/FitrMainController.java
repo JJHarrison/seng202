5cd6564b5fb5608460806ca328327897c6f4cada
dvanwichen <dpv11@uclive.ac.nz>
2014-09-01 21:09:11 +1200

    GUI changes (look and feel stuff)

--- SENG202/src/view/FitrMainController.java
+++ SENG202/src/view/FitrMainController.java
@@ -9,21 +9,30 @@ public class FitrMainController {
 
 	@FXML
 	ListView<String> eventSelector;
-
+	
+	@FXML
+	FitrTileControl mapTest;
+	
+	
+	
 	@FXML
 	private void initialize() {
+		
+		
+		
 		ObservableList<String> list = FXCollections.observableArrayList();
 		list.add("11:24 - 12:45\nWalk with Dog");
-		list.add("<S Time - F Time>\n<Event Title>");
-		list.add("<S Time - F Time>\n<Event Title>");
-		list.add("<S Time - F Time>\n<Event Title>");
-		list.add("<S Time - F Time>\n<Event Title>");
-		list.add("<S Time - F Time>\n<Event Title>");
-		list.add("<S Time - F Time>\n<Event Title>");
-		list.add("<S Time - F Time>\n<Event Title>");
-		list.add("<S Time - F Time>\n<Event Title>");
-		list.add("<S Time - F Time>\n<Event Title>");
-		list.add("<S Time - F Time>\n<Event Title>");
+		list.add("11:24 - 12:45\nWalk with Dog");
+		list.add("11:24 - 12:45\nWalk with Dog");
+		list.add("11:24 - 12:45\nWalk with Dog");
+		list.add("11:24 - 12:45\nWalk with Dog");
+		list.add("11:24 - 12:45\nWalk with Dog");
+		list.add("11:24 - 12:45\nWalk with Dog");
+		list.add("11:24 - 12:45\nWalk with Dog");
+		list.add("11:24 - 12:45\nWalk with Dog");
+		list.add("11:24 - 12:45\nWalk with Dog");
+		list.add("11:24 - 12:45\nWalk with Dog");
+		list.add("11:24 - 12:45\nWalk with Dog");
 		eventSelector.setItems(list);
 	}
 
--- SENG202/src/view/FitrTileControl.java
+++ SENG202/src/view/FitrTileControl.java
@@ -1,9 +1,14 @@
 package view;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
 public class FitrTileControl extends AnchorPane {
+	@FXML
+	WebView webViewTest;
 
 	public FitrTileControl() {
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
@@ -15,5 +20,11 @@ public class FitrTileControl extends AnchorPane {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		
+		WebEngine webEngine = webViewTest.getEngine();
+		
+		webEngine.loadContent("<img src=\"http://maps.googleapis.com/maps/api/staticmap?path=color:0x0000ff80|weight:3|30.245576,-97.823843|30.246356,-97.823326|30.246539,-97.821931|30.247105,-97.821064|30.247719,-97.820641|30.248482,-97.820708|30.24915,-97.820722&size=1000x530\">");
+		
 	}
 }
