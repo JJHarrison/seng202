5a89018d63f4d9338670e1a18a515f70ff4c839b
simoncrequer <sjc291@uclive.ac.nz>
2014-09-11 14:34:55 +1200

    Implemented Graph class -simon

--- SENG202/src/data/model/Graph.java
+++ SENG202/src/data/model/Graph.java
@@ -1,5 +1,38 @@
 package data.model;
 
-public class Graph {
+import java.util.ArrayList;
 
+public class Graph {
+	private String title;
+	private String xName;
+	private String yName;
+	ArrayList<double[]> points;
+	
+	public Graph(String title, String xName, String yName) {
+		this.title = title;
+		this.xName = xName;
+		this.yName = yName;
+		this.points = new ArrayList<double[]>();
+	}
+	
+	public void addPoint(double x, double y) {
+		double[] p = {x, y};
+		this.points.add(p);
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getXName() {
+		return xName;
+	}
+	
+	public String getYName() {
+		return yName;
+	}
+	
+	public ArrayList<double[]> getPoints() {
+		return points;
+	}
 }
