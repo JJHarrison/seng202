27a32b92b1e38cc4f252ed8623f435e161982143
sds53 <sds53@uclive.ac.nz>
2014-09-15 17:02:53 +1200

    Fixed issues

--- SENG202/src/view/analysis/AnalysisController.java
+++ SENG202/src/view/analysis/AnalysisController.java
@@ -1,6 +1,8 @@
 package view.analysis;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import data.model.DataPoint;
 import data.model.Event;
@@ -16,6 +18,31 @@ public class AnalysisController {
     @FXML
     void initialize() {
     	ArrayList<DataPoint> points = new ArrayList<DataPoint>();
+    	Calendar c3 = new GregorianCalendar(2005, // Year
+        		5, // Month
+        		10, // Day
+        		23, // Hour
+        		42, // Minute
+        		28); // Second
+        	Calendar c4 = new GregorianCalendar(2005, // Year
+        		5, // Month
+        		10, // Day
+        		23, // Hour
+        		43, // Minute
+        		5); // Second
+
+        		DataPoint p1 = new DataPoint.Builder().date(c3).heartRate(120)
+        				.latitude(30.2553368).longitude(-97.83891084).altitude(50.0)
+        				.prevDataPoint(null).build();
+
+        		DataPoint p2 = new DataPoint.Builder().date(c4).heartRate(125)
+        				.latitude(30.25499189).longitude(-97.83913958).altitude(51.0)
+        				.prevDataPoint(p1).build();
+
+        	
+
+        		points.add(p1);
+        		points.add(p2);
 	tileBox.getChildren().addAll(new Tile(new Event("Yay", points)), new Tile(new Event("Jay", points)), new Tile(new Event("Wins", points)));
     }
 
--- SENG202/src/view/tile/testerTile.java
+++ SENG202/src/view/tile/testerTile.java
@@ -18,7 +18,36 @@ public class testerTile extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-    	ArrayList<DataPoint> points = new ArrayList<DataPoint>();
+    	
+    	
+    	
+    	
+    ArrayList<DataPoint> points = new ArrayList<DataPoint>();
+    Calendar c3 = new GregorianCalendar(2005, // Year
+    		5, // Month
+    		10, // Day
+    		23, // Hour
+    		42, // Minute
+    		28); // Second
+    	Calendar c4 = new GregorianCalendar(2005, // Year
+    		5, // Month
+    		10, // Day
+    		23, // Hour
+    		43, // Minute
+    		5); // Second
+
+    		DataPoint p1 = new DataPoint.Builder().date(c3).heartRate(120)
+    				.latitude(30.2553368).longitude(-97.83891084).altitude(50.0)
+    				.prevDataPoint(null).build();
+
+    		DataPoint p2 = new DataPoint.Builder().date(c4).heartRate(125)
+    				.latitude(30.25499189).longitude(-97.83913958).altitude(51.0)
+    				.prevDataPoint(p1).build();
+
+    	
+
+    		points.add(p1);
+    		points.add(p2);
 	Event event = new Event("Yay I got this working", points);
 	//Calendar calendar = new GregorianCalendar();
 	/*event.setStartTime(calendar);
