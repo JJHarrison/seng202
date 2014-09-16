12ddd3587ab9f58f032bd2822aaf29b5a92bf7e8
Jammingz <jgr111@uclive.ac.nz>
2014-09-16 12:33:39 +1200

    Made SearchQuery class

---
+++ SENG202/src/view/web/SearchQuery.java
@@ -0,0 +1,25 @@
+package view.web;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class SearchQuery {
+
+	public static final String url = "https://duckduckgo.com/?q=";
+	
+	/**
+	 * A function that takes a plain text search query and returns a complete search request URL
+	 * 
+	 * @param inputQuery The plain text search query
+	 * @return The search request URL
+	 */
+	public static String getQuery(String inputQuery) {
+		String query = url;
+		try {
+			query.concat(URLEncoder.encode(inputQuery, "UTF=8"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return query;
+	}
+}
