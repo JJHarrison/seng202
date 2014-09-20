14a1bb4320a8f0ca2befaa0cb3638f5da974f415
sds53 <sds53@uclive.ac.nz>
2014-09-20 21:11:45 +1200

    fixed file loader

--- SENG202/src/data/loader/FileLoader.java
+++ SENG202/src/data/loader/FileLoader.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
+
 
 import data.model.DataPoint;
 import data.model.Event;
@@ -25,7 +25,7 @@ import data.model.EventContainer;
 public class FileLoader {
 	private InputStream inputStream;
 	private EventContainer eventContainer = new EventContainer();
-	public ArrayList<Event> events = new ArrayList<Event>();
+	private DataPoint lastPoint;
 
 	/**
 	 * Creates a FileLoader with a particular input file
@@ -63,75 +63,68 @@ public class FileLoader {
 	 * data points to the event
 	 */
 	public void load() {
-		BufferedReader br = null;
-		String line = "";
-		DataPoint lastPoint = null;
-		Event currentEvent = null;
+		BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+		String line;
 		ArrayList<DataPoint> points = new ArrayList<DataPoint>();
-		String currentName = null; 
-
+		String eventName = null;
+		
 		try {
-			br = new BufferedReader(new InputStreamReader(inputStream));
-
 			while ((line = br.readLine()) != null) {
 				String[] dataLine = line.split(",");
-
-				if (isValidLine(line) || line.startsWith("#start")) {
-					if (dataLine[0].contains("#start")) {
-						// we need to add create an event and add the points
-						// from before start
-					
-						if (!points.isEmpty()) {
-							
-							currentEvent = new Event(currentName, points);
-							eventContainer.addEvent(currentEvent);
-							events.add(currentEvent);
-							points.clear();
+				
+				if(isValidLine(line) || line.startsWith("#start")) {
+					if(dataLine[0].contains("#start")) { //start of new event
+						if(!points.isEmpty()) { // checks that points have been read
+							System.out.println("adding " + points.size() + " Points to event: " + eventName);
+							eventContainer.addEvent(new Event(eventName, points));
+							points = new ArrayList<DataPoint>();
 						}
-						currentName = dataLine[1];
-						lastPoint = null;
-					} else {
-						String[] dateString = dataLine[0].split("/");
-						String[] time = dataLine[1].split(":");
-	
-						// months start from 0...
-						Calendar date = new GregorianCalendar(
-								Integer.parseInt(dateString[2]), // Year
-								Integer.parseInt(dateString[1]), // Month
-								Integer.parseInt(dateString[0]), // Day
-								Integer.parseInt(time[0]), // Hour
-								Integer.parseInt(time[1]), // Minute
-								Integer.parseInt(time[2])); // Second
-
-						int heartrate = Integer.parseInt(dataLine[2]);
-						double latitude = Double.parseDouble(dataLine[3]);
-						double longitude = Double.parseDouble(dataLine[4]);
-						double altitude = Double.parseDouble(dataLine[5]);
-
-						DataPoint point = new DataPoint.Builder().date(date)
-								.heartRate(heartrate).latitude(latitude)
-								.longitude(longitude).altitude(altitude)
-								.prevDataPoint(lastPoint).build();
-
+						
+						eventName =  dataLine[1]; 
+						
+					} else { // line containing data
+						DataPoint point = parseLine(dataLine);
 						points.add(point);
-						lastPoint = point;
 					}
 				}
 			}
-			
-			if(currentName != null && points.size() != 0) {
-				currentEvent = new Event(currentName, points);
-				eventContainer.addEvent(currentEvent);
+			if(!points.isEmpty() && !points.isEmpty()) { // checks that points have been read
+				System.out.println("adding " + points.size() + " Points to event: " + eventName);
+				eventContainer.addEvent(new Event(eventName, points));
 			}
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			System.out.println("File Not Found");
+			
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-			System.out.println("couldnt read line");
 		}
-
+	}
+	
+	/**
+	 * Takes a correctly formatted line and returns it as DataPoint
+	 * @param line
+	 * @return
+	 */
+	private DataPoint parseLine(String[] line) {
+		String[] dateString = line[0].split("/");
+		String[] time = line[1].split(":");
+		
+		Calendar date = new Calendar.Builder()
+		.setDate(Integer.parseInt(dateString[2]), Integer.parseInt(dateString[1]), Integer.parseInt(dateString[0]))
+		.setTimeOfDay(Integer.parseInt(time[0]), Integer.parseInt(time[1]), Integer.parseInt(time[2]))
+		.build();
+		
+		int heartrate = Integer.parseInt(line[2]);
+		double latitude = Double.parseDouble(line[3]);
+		double longitude = Double.parseDouble(line[4]);
+		double altitude = Double.parseDouble(line[5]);
+		
+		DataPoint point = new DataPoint.Builder().date(date)
+				.heartRate(heartrate).latitude(latitude)
+				.longitude(longitude).altitude(altitude)
+				.prevDataPoint(lastPoint).build();
+		
+		lastPoint = point;
+		return point;
 	}
 
 	/**
@@ -150,23 +143,22 @@ public class FileLoader {
 	 * @return isValid
 	 */
 	public boolean isValidLine(String line) { 
-		boolean isValid = false;
+		boolean isValid = true;
 		String z = "(\\d){2}/(\\d){2}/(\\d){4},";
 		String y = "(\\d){2}:(\\d){2}:(\\d){2},";
 		String x = "(\\d){2,3},(\\-)?(\\d)+.(\\d)+,(\\-)?(\\d)+.(\\d)+,(\\d){2,3}(.(\\d))?";
 		String reg = z + y + x;
 		
-		 if(line.matches(reg)) {
-			// the line must pass the regex before checking validity of the values
-			// otherwise it will break as isInRange() does not check if it can parse the string
+		if(line.length() == 0) {
+			isValid = false;
+		} else if(!line.matches(reg)) {
 			String[] values = line.split(",");
 			if ((values.length == 6)
 				&& (isDateValid(values[0]))
 				&& (isTimeValid(values[1]))
 				&& (isHeartrateValid(values[2]))
-				&& (isLatitudeValid(values[3]))
-				&& (isLongitudeValid(values[4]))){
-				isValid = true;
+				&& (isLatitudeValid(values[3]))){
+				isValid = false;
 			}
 			
 		}
@@ -177,27 +169,12 @@ public class FileLoader {
 		inputStream = null;
 	}
 
-	/**
-	 * Checks to see if the value is in a range of [low, high] (inclusive).
-	 * There are no checks to see if the value is valid to be parsed to double,
-	 * this must be handled by the function caller.
-	 * @param value The value that will be checked to see if it is in the range
-	 * @param low The lower bound of the range
-	 * @param high The higher bound of the range
-	 * @return True if the value is in the range, false otherwise
-	 */
+	
 	public boolean isInRange(String value, double low, double high){
 		double d = Double.parseDouble(value);
 		return (d >= low && d <= high);
 	}
 	
-	/**
-	 * Check to see if the date is within a valid date format
-	 * The date that is accepted is the standard New Zealand format dd/mm/yyyy ...
-	 *  [1-31]/[1,12]/[2000,2100] 
-	 * @param date The date that will be checked
-	 * @return True if the value is a valid date, false otherwise
-	 */
 	public boolean isDateValid(String date){
 		String[] values = date.split("/");
 		return (isInRange(values[0], 1, 31)
@@ -205,12 +182,6 @@ public class FileLoader {
 			&& isInRange(values[2], 2000, 2100));
 	}
 	
-	/**
-	 * Check to see if the time is a valid time format
-	 * The time that will be accepted is hh:mm:ss [0,23]:[0,59],[0,59]
-	 * @param time The time that will be checked
-	 * @return True if the value is a valid time, false otherwise
-	 */
 	public boolean isTimeValid(String time){
 		String[] values = time.split(":");
 		return (isInRange(values[0], 0, 23))
@@ -218,47 +189,33 @@ public class FileLoader {
 				&& isInRange(values[2], 0, 59);
 	}
 	
-	/**
-	 * Checks to see if the heart rate is within a valid range of [20,220]
-	 * @param hr The heart rate that will be checked
-	 * @return True if the heart rate is valid, false otherwise
-	 */
 	public boolean isHeartrateValid(String hr){
 		return isInRange(hr, 20, 220);
 	}
 	
-	/**
-	 * Checks to see if the latitude is within a valid range of [-90,90]
-	 * @param lat The latitude that will be checked 
-	 * @return True if the latitude is valid, false otherwise
-	 */
 	public boolean isLatitudeValid(String lat){
 		String[] values = lat.split("\\.");		
 		return (isInRange(lat, -90, 90) &&  values[1].length() >= 5);
 	}
 	
-	/**
-	 * Checks to see if the longitude is within a valid range of [-180,180]
-	 * @param lng The longitude that will be checked 
-	 * @return True if the longitude is valid, false otherwise
-	 */
 	public boolean isLongitudeValid(String lng){
 		String[] values = lng.split("\\.");		
 		return (isInRange(lng, -180, 180) &&  values[1].length() >= 5);
 	}
 	
 	
+	
 	public static void main(String[] args) {
 		FileLoader f = new FileLoader();
-		/*f.load();
+		f.load();
 		
 		EventContainer eC = f.getEventContainer();
 		System.out.println(eC.getAllEvents().size());
 		for (Event e : eC.getAllEvents()) {
 			System.out.print(e.getEventName() + "\n");
 			//System.out.print(e.getStartTime().get(Calendar.MINUTE) + "\n");
-		}*/
-		String l = "10/04/2005,23:42:28,69,30.2553368,-97.83891084,50";
-		System.out.println(f.isValidLine(l));
+		}
+		//String l = "10/04/2005,23:42:28,69,30.2553368,-97.83891084,239.5";
+		//System.out.println(f.isValidLine(l));
 	}
 }
