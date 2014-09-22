73f79d8441f51daf6f6583c4f81427c07d6755bf
dvanwichen <dpv11@uclive.ac.nz>
2014-09-22 18:13:53 +1200

    #

--- SENG202/src/resources/Reference.java
+++ SENG202/src/resources/Reference.java
@@ -3,7 +3,7 @@ package resources;
 /**
  * A class to reference resources (e.g. Reference.class.getResourceAsStream("bradycardia.txt"))
  * 
- * @author daniel
+ * @author Daniel van Wichen
  *
  */
 public class Reference {
