b45ec021f0c99e3a20eb3fc34481a94090aafb10
jjharrison-uc <jjh72@uclive.ac.nz>
2014-09-22 15:36:25 +1200

    .

--- SENG202/src/data/loader/FileLoader.java
+++ SENG202/src/data/loader/FileLoader.java
@@ -7,6 +7,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -153,66 +155,112 @@ public class FileLoader {
 	 * @param line
 	 * @return isValid
 	 */
-	public boolean isValidLine(String line) {
-		boolean isValid = true;
-		String z = "(\\d){2}/(\\d){2}/(\\d){4},";
-		String y = "(\\d){2}:(\\d){2}:(\\d){2},";
-		String x = "(\\d){2,3},(\\-)?(\\d)+.(\\d)+,(\\-)?(\\d)+.(\\d)+,(\\d){2,3}(.(\\d))?";
-		String reg = z + y + x;
-
-		if (line.length() == 0) {
-			isValid = false;
-		} else if (!line.matches(reg)) {
+ 	public boolean isValidLine(String line) { 
+		boolean isValid = false;
+ 		String z = "(\\d){2}/(\\d){2}/(\\d){4},";
+ 		String y = "(\\d){2}:(\\d){2}:(\\d){2},";
+ 		String x = "(\\d){2,3},(\\-)?(\\d)+.(\\d)+,(\\-)?(\\d)+.(\\d)+,(\\d){2,3}(.(\\d))?";
+ 		String reg = z + y + x;
+ 		
+		if(line.matches(reg)) {
+			// the line must pass the regex before checking validity of the values
+			// otherwise it will break as isInRange() does not check if it can parse the string
 			String[] values = line.split(",");
-			if ((values.length == 6) && (isDateValid(values[0]))
-					&& (isTimeValid(values[1]))
-					&& (isHeartrateValid(values[2]))
-					&& (isLatitudeValid(values[3]))) {
-				isValid = false;
-			}
-
+ 			if ((values.length == 6)
+ 				&& (isDateValid(values[0]))
+ 				&& (isTimeValid(values[1]))
+ 				&& (isHeartrateValid(values[2]))
+				&& (isLatitudeValid(values[3]))
+				&& (isLongitudeValid(values[4]))){
+				isValid = true;
+ 			}
 		}
 		return isValid;
-	}
+ 		}
 
 	private void clearStream() {
 		inputStream = null;
 	}
 
-	public boolean isInRange(String value, double low, double high) {
-		double d = Double.parseDouble(value);
-		return (d >= low && d <= high);
-	}
+	/**
+	* Checks to see if the value is in a range of [low, high] (inclusive).
+	* There are no checks to see if the value is valid to be parsed to double,
+	* this must be handled by the function caller.
+	* @param value The value that will be checked to see if it is in the range
+	* @param low The lower bound of the range
+	* @param high The higher bound of the range
+	* @return True if the value is in the range, false otherwise
+	*/
+ 	public boolean isInRange(String value, double low, double high){
+ 		double d = Double.parseDouble(value);
+ 		return (d >= low && d <= high);
+ 	}
 
+ 	/**
+ 	* Check to see if the date is within a valid date format
+ 	* The date that is accepted is the standard New Zealand format dd/mm/yyyy ...
+ 	*  [1-31]/[1,12]/[2000,Current year] 
+ 	* @param date The date that will be checked
+ 	* @return True if the value is a valid date, false otherwise
+ 	*/
 	public boolean isDateValid(String date) {
 		String[] values = date.split("/");
-		return (isInRange(values[0], 1, 31) && isInRange(values[1], 1, 12) && isInRange(
-				values[2], 2000, 2100));
+		SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+		df.setLenient(false);
+		try {
+	        df.parse(date);
+	        return (isInRange(values[2], 2000, Calendar.getInstance().get(Calendar.YEAR)));
+	    } catch (ParseException e) {
+	        return false;
+	    }
 	}
+	
 
-	public boolean isTimeValid(String time) {
-		String[] values = time.split(":");
-		return (isInRange(values[0], 0, 23)) && isInRange(values[1], 0, 59)
-				&& isInRange(values[2], 0, 59);
-	}
+	/**
+	* Check to see if the time is a valid time format
+	* The time that will be accepted is hh:mm:ss [0,23]:[0,59],[0,59]
+	* @param time The time that will be checked
+	* @return True if the value is a valid time, false otherwise
+	*/
+ 	public boolean isTimeValid(String time){
+ 		String[] values = time.split(":");
+ 		return (isInRange(values[0], 0, 23))
+ 				&& isInRange(values[1], 0, 59)
+ 				&& isInRange(values[2], 0, 59);
+ 	}
 
-	public boolean isHeartrateValid(String hr) {
-		return isInRange(hr, 0, 220);
-	}
+	/**
+	* Checks to see if the heart rate is within a valid range of [20,220]
+	* @param hr The heart rate that will be checked
+	* @return True if the heart rate is valid, false otherwise
+	*/
+ 	public boolean isHeartrateValid(String hr){
+ 		return isInRange(hr, 20, 220);
+ 	}
 
-	public boolean isLatitudeValid(String lat) {
-		String[] values = lat.split("\\.");
-		return (isInRange(lat, -90, 90) && values[1].length() >= 5);
-	}
+	/**
+	* Checks to see if the latitude is within a valid range of [-90,90]
+	* @param lat The latitude that will be checked 
+	* @return True if the latitude is valid, false otherwise
+	*/
+ 	public boolean isLatitudeValid(String lat){
+ 		String[] values = lat.split("\\.");		
+ 		return (isInRange(lat, -90, 90) &&  values[1].length() >= 5);
+ 	}
 
-	public boolean isLongitudeValid(String lng) {
-		String[] values = lng.split("\\.");
-		return (isInRange(lng, -180, 180) && values[1].length() >= 5);
-	}
+	/**
+	* Checks to see if the longitude is within a valid range of [-180,180]
+	* @param lng The longitude that will be checked 
+	* @return True if the longitude is valid, false otherwise
+	*/
+ 	public boolean isLongitudeValid(String lng){
+ 		String[] values = lng.split("\\.");		
+ 		return (isInRange(lng, -180, 180) &&  values[1].length() >= 5);
+ 	}
 
 	public static void main(String[] args) {
 		FileLoader f = new FileLoader();
-		f.load();
+		/*f.load();
 
 		EventContainer eC = f.getEventContainer();
 		System.out.println("----" + eC.getLastDate());
@@ -223,5 +271,7 @@ public class FileLoader {
 					+ "/" + e.getStartTime().get(Calendar.MONTH) + "/"
 					+ e.getStartTime().get(Calendar.YEAR));
 		}
+		*/
+		
 	}
 }
