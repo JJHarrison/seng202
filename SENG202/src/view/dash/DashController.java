38204c0da7156a782375d5f7775a2b0dc951dab3
dvanwichen <dpv11@uclive.ac.nz>
2014-09-23 15:41:47 +1200

    #Daniel added GUI functionality to dashboard totals

--- SENG202/src/view/dash/DashController.java
+++ SENG202/src/view/dash/DashController.java
@@ -1,18 +1,22 @@
 package view.dash;
 
+import java.util.Calendar;
+
 import resources.Reference;
 import user.User.Gender;
+import data.model.Summary;
 import data.persistant.Persistent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
 public class DashController {
 
 	@FXML
 	ImageView imageGender;
-	
+
 	@FXML
 	Label labelName;
 	@FXML
@@ -20,18 +24,55 @@ public class DashController {
 	@FXML
 	Label labelHeight;
 	@FXML
-	Label labelWeight; 
+	Label labelWeight;
 	@FXML
 	Label labelHR;
 	@FXML
 	Label labelBMI;
+
+	@FXML
+	Label totalKmLabel;
+	@FXML
+	Label totalHoursLabel;
+	@FXML
+	Label totalCaloriesLabel;
+
+	@FXML
+	Label monthKmLabel;
+	@FXML
+	Label monthHoursLabel;
+	@FXML
+	Label monthCaloriesLabel;
+
+	@FXML
+	Label achieveMaxSpeed;
+	@FXML
+	Label achieveMaxDistance;
+	@FXML
+	Label achieveMaxHours;
+
+	@FXML
+	VBox warningPane;
 	
+	private Summary summaryTotal;
+	private Summary summaryMonth;
+
 	@FXML
 	void initialize() {
+		fillUser();
+		fillDash();
+	}
+
+	/**
+	 * Fills the user information into the dashboard
+	 */
+	private void fillUser() {
 		if (Persistent.getCurrentUser().getGender() == Gender.MALE) {
-			imageGender.setImage(new Image(Reference.class.getResourceAsStream("male.png")));
+			imageGender.setImage(new Image(Reference.class
+					.getResourceAsStream("male.png")));
 		} else {
-			imageGender.setImage(new Image(Reference.class.getResourceAsStream("female.png")));
+			imageGender.setImage(new Image(Reference.class
+					.getResourceAsStream("female.png")));
 		}
 		labelName.setText(Persistent.getCurrentUser().getName());
 		labelAge.setText(Integer.toString(Persistent.getCurrentUser().getAge()));
@@ -39,7 +80,41 @@ public class DashController {
 		labelWeight.setText(String.format("%.0f", Persistent.getCurrentUser().getWeight()) + " kg");
 		labelHR.setText(String.format("%d", Persistent.getCurrentUser().getRestingHeartRate()));
 		labelBMI.setText(String.format("%.0f", Persistent.getCurrentUser().getBMI()));
+	}
+	
+	private void fillMonth() {
+		Calendar to = Calendar.getInstance();
+		Calendar from = Calendar.getInstance();
+		from.add(Calendar.MONTH, -1);
 		
+		summaryMonth = new Summary(Persistent.getCurrentUser().getEvents(), from, to);
+		monthKmLabel.setText(summaryMonth.getTotalDistance());
+		monthCaloriesLabel.setText(summaryMonth.getTotalCalories());
+		monthHoursLabel.setText(summaryMonth.getTotalDuration());
+	}
+	
+	private void fillTotal() {
+		summaryTotal = new Summary(Persistent.getCurrentUser().getEvents(), null, null);
+		totalKmLabel.setText(summaryTotal.getTotalDistance());
+		totalCaloriesLabel.setText(summaryTotal.getTotalCalories());
+		totalHoursLabel.setText(summaryTotal.getTotalDuration());
+		fillAchievements();
+	}
+	
+	private void fillAchievements() {
+		System.out.print(summaryTotal.getMaxDistance());
+		System.out.print(Persistent.getCurrentUser());
+		achieveMaxDistance.setText(summaryTotal.getMaxDistance());
+		achieveMaxHours.setText(summaryTotal.getMaxDuration());
+		achieveMaxSpeed.setText(summaryTotal.maxSpeed());
+	}
+	
+	/**
+	 * Fill the dash board with all the totals and achievements.
+	 */
+	public void fillDash() {
+		fillMonth();
+		fillTotal();
 	}
 
 }
