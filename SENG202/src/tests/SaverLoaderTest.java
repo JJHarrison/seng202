7d08e98f782392436053ee1ef2933caa8d6682e9
sds53 <sds53@uclive.ac.nz>
2014-09-23 19:20:26 +1200

    #

--- SENG202/src/tests/SaverLoaderTest.java
+++ SENG202/src/tests/SaverLoaderTest.java
@@ -1,13 +1,19 @@
 package tests;
 
+import java.io.File;
+
 import junit.framework.TestCase;
 import user.User;
+import data.persistant.Loader;
 import data.persistant.Persistent;
 import data.persistant.Saver;
 
 public class SaverLoaderTest extends TestCase {
 
-	
+	/**
+	 * Tests saving a user to json 
+	 * @throws Exception
+	 */
 	public void testSaveUser() throws Exception {
 		Persistent.setFilePath("/Users/SamSchofield/Desktop");
 		User u = User.mockUser();
@@ -16,9 +22,12 @@ public class SaverLoaderTest extends TestCase {
 		Saver.SaveUser(u);
 	}
 
-		
+	/**
+	 * checks that the user that was loaded is the same as the one which was saved
+	 */
 	public void testLoader() {
-		 //User u = Loader.loadUserProfile(new File(Persistent.getProfileFilePath(Persistent.getCurrentUser().getUserId())));		 
+		 User u = Loader.loadUserProfile(new File(Persistent.getProfileFilePath(Persistent.getCurrentUser().getUserId())));		 
+		 assertEquals(u, User.mockUser());
 	}
 	
 	
