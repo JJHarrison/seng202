e53d1c82ccc9379806299d3aca785ddf3c112000
dvanwichen <dpv11@uclive.ac.nz>
2014-09-23 17:34:29 +1200

    #

--- SENG202/src/tests/SummaryTest.java
+++ SENG202/src/tests/SummaryTest.java
@@ -11,10 +11,10 @@ import data.model.Summary;
 import junit.framework.TestCase;
 
 public class SummaryTest extends TestCase {
-	
+
 	private EventContainer eventContainer;
 	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	
+
 	/**
 	 * sets up the event container loaded from the default csv file
 	 */
@@ -25,42 +25,46 @@ public class SummaryTest extends TestCase {
 		fl.load();
 		eventContainer = fl.getEventContainer();
 	}
-	
+
 	/**
-	 * checks that the constructor is correctly identifying the events between the start and end date
+	 * checks that the constructor is correctly identifying the events between
+	 * the start and end date
+	 * 
 	 * @throws ParseException
 	 */
 	public void testSummary() throws ParseException {
-		Calendar startDate = new Calendar.Builder().setInstant(sdf.parse("10/04/2005")).build();
-		Calendar endDate = new Calendar.Builder().setInstant(sdf.parse("12/04/2005")).build();
-		Summary s = new Summary(eventContainer,startDate, endDate);
+		Calendar startDate = new Calendar.Builder().setInstant(
+				sdf.parse("10/04/2005")).build();
+		Calendar endDate = new Calendar.Builder().setInstant(
+				sdf.parse("12/04/2005")).build();
+		Summary s = new Summary(eventContainer, startDate, endDate);
 		// there are 3 events between the specified dates
 		assertEquals(3, s.getNumberOfEvents());
 	}
-	
+
 	/**
-	 * checks that the constructor is correctly identifying the events between the start and end date
+	 * checks that the constructor is correctly identifying the events between
+	 * the start and end date
+	 * 
 	 * @throws ParseException
 	 */
 	public void testCalculate() throws ParseException {
-		Calendar startDate = new Calendar.Builder().setInstant(sdf.parse("11/04/2005")).build();
-		Calendar endDate = new Calendar.Builder().setInstant(sdf.parse("11/04/2005")).build();
-		Summary s = new Summary(eventContainer,null, null);
-		
+		Summary s = new Summary(eventContainer, null, null);
+
 		System.out.println(s.getNumberOfEvents());
 		for (Event e : s.getEvents()) {
 			System.out.println(e.getEventName());
 		}
-		
+
 		/*
-		 * this part is to test that there were no errors in the formatting 
+		 * this part is to test that there were no errors in the formatting
 		 */
 		System.out.println("\n");
 		System.out.println("totals:");
 		System.out.println(s.getTotalDuration());
 		System.out.println(s.getTotalDistance());
 		System.out.println(s.getTotalCalories());
-		
+
 		System.out.println("\nMaximums:");
 		System.out.println(s.getMaxDuration());
 		System.out.println(s.maxCalories());
@@ -68,18 +72,19 @@ public class SummaryTest extends TestCase {
 		System.out.println(s.maxCalories());
 		System.out.println(s.maxSpeed());
 		System.out.println(s.maxHeartRate());
-		
-		
+
 	}
-	
+
 	/**
-	 * checks that the constructor is correctly identifying the events between the start and end date
+	 * checks that the constructor is correctly identifying the events between
+	 * the start and end date
+	 * 
 	 * @throws ParseException
 	 */
 	public void testNullDates() throws ParseException {
-		// there should be 12 events in total 
-		Summary s = new Summary(eventContainer,null, null);
+		// there should be 12 events in total
+		Summary s = new Summary(eventContainer, null, null);
 		assertEquals(12, s.getNumberOfEvents());
 	}
-	
+
 }
--- SENG202/src/view/search/SearchController.java
+++ SENG202/src/view/search/SearchController.java
@@ -3,15 +3,14 @@ package view.search;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-
 public class SearchController {
 
 	@FXML
 	Label labelTitle;
-	
+
 	@FXML
 	Label labelURL;
-	
+
 	/**
 	 * Fill the label and URL for the search tile
 	 */
@@ -19,7 +18,5 @@ public class SearchController {
 		labelTitle.setText("Insert Useful Health Result");
 		labelURL.setText("www.heartdiseasefordummies.org");
 	}
-	
 
 }
-
