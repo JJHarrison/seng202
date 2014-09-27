2e9fbf95e0f840116e5644afd8e82cb6f84a3477
sds53 <sds53@uclive.ac.nz>
2014-09-27 17:44:13 +1200

    UsedID is now a random number represented as a string

--- SENG202/src/data/persistant/Persistent.java
+++ SENG202/src/data/persistant/Persistent.java
@@ -4,8 +4,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.UUID;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
+
+import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -73,7 +76,7 @@ public class Persistent {
 	 * user profile sub directories are named as the users ID number
 	 * @return profileFilePath
 	 */
-	public static String getProfileFilePath(int userID) {
+	public static String getProfileFilePath(String userID) {
 		return getFilePath() + userID + "/." + userID + ".fitr";
 	}
 
@@ -109,7 +112,7 @@ public class Persistent {
 		boolean userAdded;
 		
 		if (!users.contains(user)) {
-			int userID = user.getUserId();
+			String userID = user.getUserId();
 			new File(getFilePath() + userID).mkdir();
 
 			try {
@@ -121,7 +124,7 @@ public class Persistent {
 			users.add(user);
 			userNames.add(user.getName());
 			Saver.SaveUser(user); // automatically save the user to json once they have been added 
-			prefs.putInt("UserID", prefs.getInt("UserID", 0) + 1);
+			prefs.put("UserID", generateUserID());
 			userAdded = true;
 		} else {
 			userAdded = false;
@@ -130,14 +133,24 @@ public class Persistent {
 		}
 		return userAdded;
 	}
+	
+	/**
+	 * generates a unique UserID using UUID
+	 * @return unique userID
+	 */
+	private static String generateUserID() {
+		String userID = UUID.randomUUID().toString();
+		System.out.println(userID);
+		return userID;
+	}
 
 	/**
 	 * returns the most recently used userID.
 	 * used in generating userID values 
 	 * @return userId
 	 */
-	public static int getUserID() {
-		return prefs.getInt("UserID", 0);
+	public static String getUserID() {
+		return prefs.get("UserID", null);
 	}
 
 	/**
@@ -218,7 +231,6 @@ public class Persistent {
 		try {
 			prefs.clear();
 		} catch (BackingStoreException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
