e4aeb6e4777641c61a3b34604e17c16d2050dc6b
Daniel Tapp <djt104@uclive.ac.nz>
2014-10-04 13:46:57 +1300

    changes to serverController, trying to work out the tasks.

--- SENG202/src/view/server/ServerController.java
+++ SENG202/src/view/server/ServerController.java
@@ -7,7 +7,6 @@ package view.server;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import server.Server;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
@@ -18,6 +17,7 @@ import javafx.scene.control.TextArea;
 public class ServerController implements Initializable {
 
 	String consoleText = "";
+	
 	private Server s = new Server();
 
 	@FXML
@@ -25,31 +25,45 @@ public class ServerController implements Initializable {
 	
 	Boolean serverRunning = false;
 
+	/**
+	 * Creates a new task that contains the server.
+	 * This way the interface wont lag when the server is operating.
+	 */
+	Task<Void> t = new Task<Void>() {
+		@Override
+		protected Void call() throws Exception {
+			s.startServer();	
+			if (isCancelled()) {
+				System.out.println("cancelled");
+				s.shutDownServer();
+			}
+			return null;
+		}
+	};
+	
+	/**
+	 * Actions taken when the 'START' button is used.
+	 */
 	@FXML
 	void actionStart() {
 		if (serverRunning) {
 			setConsoleText("The server is already running.");
 		} else {			
 			setConsoleText("Server Started!");
-			Task<Void> t = new Task<Void>() {
 
-				@Override
-				protected Void call() throws Exception {
-					// TODO Auto-generated method stub
-					s.startServer();
-					return null;
-				}
-			};
 			Thread thread = new Thread(t);
 			thread.start();
 			serverRunning = true;
 		}
 	}
 	
+	/**
+	 * Actions taken when the 'STOP' button is used.
+	 */
 	@FXML
 	void actionStop() {
 		if (serverRunning) {
-			s.shutDownServer();
+			t.cancel();
 			setConsoleText("Server Stopped.");
 			serverRunning = false;
 		} else {
@@ -57,12 +71,18 @@ public class ServerController implements Initializable {
 		}
 	}
 
+	/**
+	 * Adds a new line to the console output on the interface with the passed String
+	 */
 	public void setConsoleText(String newLine) {
 		consoleText = consoleText + newLine + '\n';
 		textConsole.setText(consoleText);
 		scrolldown();
 	}
 	
+	/**
+	 * Scrolls the console window to the bottom to keep the latest line in focus
+	 */
 	private void scrolldown() {
 		textConsole.setScrollTop(Double.MAX_VALUE);
 	}
