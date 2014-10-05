00028373603912bd1344b04af8936057745c6426
sds53 <sds53@uclive.ac.nz>
2014-10-05 18:37:21 +1300

    Added a save method to allow the user to save their profile picture

--- SENG202/src/data/persistant/Saver.java
+++ SENG202/src/data/persistant/Saver.java
@@ -1,12 +1,16 @@
 package data.persistant;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-
+import java.io.InputStream;
+import java.io.OutputStream;
 import user.User;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
 
 /**
  * A Saver class used for saving an event container to a JSON file
@@ -30,7 +34,32 @@ public class Saver {
 			writer.close();
 		} catch (IOException e) {
 			System.out.println("FilePath does not exist");
-			e.printStackTrace();
+			//e.printStackTrace();
 		}
 	}
+	
+	/**
+	 * saves a profile picture (file) that the user has imported to the users directory
+	 * @param picture the picture to be saved
+	 * @param user the user who the profile picture is for
+	 * @throws IOException
+	 */
+	public static void SaveProfilePicture(File picture, User user) throws IOException {
+		InputStream input = null;
+		OutputStream output = new FileOutputStream(Persistent.getProfileFilePath(user.getUserId()));
+		try {
+	    	  input = new FileInputStream(picture);
+	    	  byte[] buf = new byte[1024];
+	    	  int bytesRead;
+	    	  while ((bytesRead = input.read(buf)) > 0) {
+	    		  output.write(buf, 0, bytesRead);
+	    	  }
+	    	  
+		} finally {
+			input.close();
+			output.close();
+		}
+
+	}
+
 }
