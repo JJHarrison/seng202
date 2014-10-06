49bdb8f4447bcbaf983624f5eb5921c68cb9b7eb
Daniel Tapp <djt104@uclive.ac.nz>
2014-10-06 21:48:00 +1300

    commenting for notifications

--- SENG202/src/view/notification/Notification.java
+++ SENG202/src/view/notification/Notification.java
@@ -1,13 +1,17 @@
 package view.notification;
 
+/**
+ * 
+ * @author Daniel Tapp
+ *
+ */
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import view.Main;
-
-
 
 public class Notification {
 
@@ -17,16 +21,21 @@ public class Notification {
 		
 		primaryStage = stage;
 		
+		// Create the Window
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(getClass().getResource("Notification.java"));
 			Parent root = loader.load(getClass().getResourceAsStream("Notification.fxml"));
 			NotificationController controller = loader.getController();
 			
+			// Fills the labels on the notification panel with the appropriate data
 			controller.fill();
 			
 			Scene scene = new Scene(root);
 			
+			
+			// Makes it so that the window acts like a pop-up
+			// (Cannot use the program until 'Ok' is clicked)
 			primaryStage.initOwner(Main.stage);
 			primaryStage.setScene(scene);
 			primaryStage.initModality(Modality.WINDOW_MODAL);
@@ -35,10 +44,12 @@ public class Notification {
 			e.printStackTrace();
 		}
 		
+		// Set the size of the window
 		primaryStage.setMinWidth(300);
 		primaryStage.setMinHeight(260);
 		primaryStage.setResizable(false);
 		
+		// Show the window
 		primaryStage.show();
 
 	}
